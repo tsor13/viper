823c460e42d1d92136cfc4c1fed03ce1c1a768de
tsor13 <tsor1313@gmail.com>
2020-06-12 17:27:00 -0600

    repeat action

--- build/example_simulate.py
+++ build/example_simulate.py
@@ -9,8 +9,9 @@ action_min, action_max = .2, 2
 
 with open('output.txt', 'w') as file:
     for i in range(1400):
-        rand = np.random.rand(3)
-        action = action_min * rand + (1-rand) * action_max
+        # rand = np.random.rand(3)
+        # action = action_min * rand + (1-rand) * action_max
+        action = np.array([1.5, 1.5, .2])
         state, reward = env.step(action)
         combined = np.hstack([state, action])
         row = ' '.join([str(f) for f in combined])
