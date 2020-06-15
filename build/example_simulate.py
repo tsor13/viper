72450443fe6f8a4c0c65ff91f92c3352105b50b8
tsor13 <tsor1313@gmail.com>
2020-06-15 12:04:09 -0600

    shortened simulation time to a minute

--- build/example_simulate.py
+++ build/example_simulate.py
@@ -9,7 +9,7 @@ s = env.reset()
 action_min, action_max = .2, 2
 
 with open('output.txt', 'w') as file:
-    for i in tqdm(range(20000)):
+    for i in tqdm(range(3600)):
         rand = np.random.rand(3)
         action = action_min * rand + (1-rand) * action_max
         # action = np.array([1.5, 1.5, .2])
@@ -18,4 +18,6 @@ with open('output.txt', 'w') as file:
         row = ' '.join([str(f) for f in combined])
         file.write(row)
         file.write('\n')
+        if i == 1800:
+            s  = env.reset()
     file.close()
