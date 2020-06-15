5f7c0c766fa3ff271a0dab86b848813e56eefe4a
tsor13 <tsor1313@gmail.com>
2020-06-15 15:13:26 -0600

    Enriched environment class

--- build/ViperEnvironment.py
+++ build/ViperEnvironment.py
@@ -5,13 +5,39 @@ import pdb
 class ViperEnvironment:
     def __init__(self):
         self.env = interface.Environment()
+        # reset environment to inital position
         self.env.reset()
+
+        # sizes of action and state vector
+        self.action_size = 3
+        self.state_size = 1404
+
+        # action bounds
+        self.action_max = 2
+        self.action_min = .2
+
+        self.time_step = 0
+        self.max_steps = 2000
+
     def step(self, action):
-        assert len(action) == 3
-        assert action.max() <= 2 and action.min() >= .2
+        '''
+        Step 1/60th of a second with action.
+        Action is either a 3-size numpy array or length 3 iterable.
+        Each element of action represents a contraction or extending of a thread of tentacle pills.
+        Less than 1 is a contraction, 1 is the original length, and greater than 1 is an extension.
+        '''
+        action = np.array(action)
+        if len(action) != 3:
+            raise Exception(f'Action must be of size {self.action_size}')
+        if action.max() > 2 or action.min() < .2:
+            raise Exception(f'Action values must be within {self.action_min} and {self.action_max}')
+        # step
         self.env.step(*action)
+
+        # STEP
         state = np.array([self.env.get_state(i) for i in range(1404)])
 
+        # REWARD
         # penalty is euclidean distance between tip of tentacle and point on cube
         tentacle_index = 12 * 89
         tentacle_point = state[tentacle_index:tentacle_index+3]
@@ -19,8 +45,27 @@ class ViperEnvironment:
         cube_point = state[cube_point:cube_point+3]
         dist = ((tentacle_point - cube_point)**2).mean()
         reward = -dist
-        return state, reward
+
+        # TERMINAL
+        self.time_step += 1
+        done = self.time_step >= self.max_steps
+
+        return state, reward, done
+
     def reset(self):
+        ''' 
+        Resets environment to random initial position.
+        The initial velocity of the tentacle and position of the cube is random
+        '''
         self.env.reset()
-        state = np.array([self.env.get_state(i) for i in range(1404)])
+        self.time_step = 0
+        state = np.array([self.env.get_state(i) for i in range(self.state_size)])
         return state
+
+    def __repr__(self):
+        s = f'Viper Environment: t = {self.time_step}'
+        return s
+
+    def __str__(self):
+        s = f'Viper Environment: t = {self.time_step}'
+        return s
