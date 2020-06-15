0b365953c2a220b4f6c5b7c6aa3d261c25c1dcc6
tsor13 <tsor1313@gmail.com>
2020-06-15 12:58:24 -0600

    added reward distribution view

--- build/example_simulate.py
+++ build/example_simulate.py
@@ -8,6 +8,7 @@ s = env.reset()
 
 action_min, action_max = .2, 2
 
+# do one minute long simulation, write data
 with open('output.txt', 'w') as file:
     rand = np.random.rand(3)
     action = action_min * rand + (1-rand) * action_max
@@ -24,3 +25,22 @@ with open('output.txt', 'w') as file:
         if i == 1800:
             s  = env.reset()
     file.close()
+
+# check reward distribution for trial
+# rewards = []
+# for _ in tqdm(range(50)):
+#     for i in range(1000):
+#         rand = np.random.rand(3)
+#         action = action_min * rand + (1-rand) * action_max
+#         s = env.reset()
+#         if i % 50:
+#             rand = np.random.rand(3)
+#             action = action_min * rand + (1-rand) * action_max
+#         state, reward = env.step(action)
+#         rewards.append(reward)
+# 
+# rewards = np.array(rewards)
+# quantiles = [np.quantile(rewards, q) for q in np.linspace(0, 1, 5)]
+# print(quantiles)
+# pdb.set_trace()
+# a = 3
