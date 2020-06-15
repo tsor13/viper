1c0b240bb6f57b8953bb86fe9159a19ecae64a6d
tsor13 <tsor1313@gmail.com>
2020-06-15 11:54:22 -0600

    simulate longer, random actions

--- build/example_simulate.py
+++ build/example_simulate.py
@@ -1,5 +1,6 @@
 import numpy as np
 from ViperEnvironment import ViperEnvironment
+from tqdm import tqdm
 import pdb
 
 env = ViperEnvironment()
@@ -8,10 +9,10 @@ s = env.reset()
 action_min, action_max = .2, 2
 
 with open('output.txt', 'w') as file:
-    for i in range(1400):
-        # rand = np.random.rand(3)
-        # action = action_min * rand + (1-rand) * action_max
-        action = np.array([1.5, 1.5, .2])
+    for i in tqdm(range(20000)):
+        rand = np.random.rand(3)
+        action = action_min * rand + (1-rand) * action_max
+        # action = np.array([1.5, 1.5, .2])
         state, reward = env.step(action)
         combined = np.hstack([state, action])
         row = ' '.join([str(f) for f in combined])
